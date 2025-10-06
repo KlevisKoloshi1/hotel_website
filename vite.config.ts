03cc700d6c7a007b4da042f207f53e83fb93abcd
KlevisKoloshi1 <kleviskoloshi@2rmlab.net>
2025-10-06 15:24:55 +0200

    vite config.ts change

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE || "/",
+  base: process.env.VITE_BASE || (mode === "production" ? "/hotel_website/" : "/"),
   server: {
     host: "::",
     port: 8080,
